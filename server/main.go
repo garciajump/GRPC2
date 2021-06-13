2147eb1d2b4f4036e82712c879dda0e8370c767f
garciajump <garciavolg1016@gmail.com>
2021-06-13 22:31:29 +0530

    deployment config changes

--- server/main.go
+++ server/main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net"
+	"fmt"   /*implements formatted I/O */
+	"log"   /* implements a simple logging package. It defines a type, Logger, with methods for formatting output */
+	"net"   /* provides a portable interface for network I/O */
 
 	"grpc-server/chat"
 
-	"google.golang.org/grpc"
+	"google.golang.org/grpc"  /*install grpc go package to fetch neccessary dependencies */
 )
 
 func main() {
 
 	fmt.Println("Go gRPC Beginners Tutorial!")
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 50051))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 50051))   /* Sprintf() function in Go language formats according to a format specifier and returns the resulting string.*/
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
 	s := chat.Server{}
 
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer()    /* NewServer creates a gRPC server which has no service registered and has not started to accept requests yet. */
 
 	chat.RegisterChatServiceServer(grpcServer, &s)
 
 	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %s", err)
+		log.Fatalf("failed to serve: %s", err)  /* Fatalf is equivalent to Printf() followed by a call to os.Exit(1).*/
 	}
 }
